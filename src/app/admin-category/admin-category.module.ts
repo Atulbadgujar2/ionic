e00d8dc06a2d59d030acaa0833b89e5f8e7b2cf6
Jayant Jape <jayant.jape@orangeneurosciences.com>
2022-06-22 11:07:48 +0530

    changes

--- src/app/admin-category/admin-category.module.ts
+++ src/app/admin-category/admin-category.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AdminCategoryRoutingModule } from './admin-category-routing.module';
 import { AdminCategoryAddComponent } from './admin-category-add/admin-category-add.component';
+import { AdminCategoryComponent } from './admin-category.component';
 
 
 @NgModule({
-  declarations: [AdminCategoryAddComponent],
+  declarations: [AdminCategoryComponent,AdminCategoryAddComponent],
   imports: [
     CommonModule,
     AdminCategoryRoutingModule
